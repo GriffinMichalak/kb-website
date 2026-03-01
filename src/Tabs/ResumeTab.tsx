273e8b13abff937709ced2cccc61cf05d37bd64b
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2026-03-01 14:31:04 -0500

    update resume link

--- src/Tabs/ResumeTab.tsx
+++ src/Tabs/ResumeTab.tsx
@@ -5,7 +5,7 @@ export const ResumeTab = () => {
   return (
     // 1206 × 1774
     <iframe
-      src="https://docs.google.com/document/d/1hfgNl0tjypAVdCTxigYtdh5oELRoM4a-3UdQwqhwVMM/edit?tab=t.0?embedded=true"
+      src="https://docs.google.com/document/d/1veqBOO1-B9lLvew1TsQuRyw945VmSq5NrqaVjl1BZ9Q/edit?tab=t.0embedded=true"
       width="100%"
       height="500px"
       style={{ border: 'none' }}
