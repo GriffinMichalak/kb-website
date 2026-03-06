71c7ba08cd8952afefb8017573730f6f5e8d2a57
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2026-03-06 15:38:10 -0500

    resume loader ff

--- src/Tabs/ResumeTab.tsx
+++ src/Tabs/ResumeTab.tsx
@@ -1,12 +1,26 @@
 import '../App.scss';
 import Loader from '../Components/Loader';
 
+const FF_RESUME_LOADER: boolean = false;
+
 export const ResumeTab = () => {
   const RESUME_LINK =
     'https://docs.google.com/document/d/1veqBOO1-B9lLvew1TsQuRyw945VmSq5NrqaVjl1BZ9Q/edit?tab=t.0embedded=true';
 
-  return (
-    <Loader align="top" text="Loading Resume...">
+  if (FF_RESUME_LOADER) {
+    return (
+      <Loader align="top" text="Loading Resume...">
+        <iframe
+          src={RESUME_LINK}
+          width="100%"
+          height="500px"
+          title="Google Doc"
+          style={{ border: 'none' }}
+        ></iframe>
+      </Loader>
+    );
+  } else {
+    return (
       <iframe
         src={RESUME_LINK}
         width="100%"
@@ -14,6 +28,6 @@ export const ResumeTab = () => {
         title="Google Doc"
         style={{ border: 'none' }}
       ></iframe>
-    </Loader>
-  );
+    );
+  }
 };
