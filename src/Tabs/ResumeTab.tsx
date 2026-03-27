ee54003c9a85b38516d14511b18a11cb3487ffcd
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2026-03-27 19:23:42 -0400

    fix error

--- src/Tabs/ResumeTab.tsx
+++ src/Tabs/ResumeTab.tsx
@@ -4,8 +4,7 @@ import { experiences, otherMediaExperience } from './resumeData';
 import { ExperienceCard } from './ExperienceCard';
 
 export const ResumeTab = () => {
-  const RESUME_LINK =
-    'https://docs.google.com/document/d/1veqBOO1-B9lLvew1TsQuRyw945VmSq5NrqaVjl1BZ9Q/edit?tab=t.0embedded=true';
+  // const RESUME_LINK = 'https://docs.google.com/document/d/1veqBOO1-B9lLvew1TsQuRyw945VmSq5NrqaVjl1BZ9Q/edit?tab=t.0embedded=true';
 
   return (
     <div>
